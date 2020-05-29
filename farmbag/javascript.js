e6338c9f25fb192c4fcf05a7fb3af7cbd1b5ac21
Ndubuisi <57354324+ndkelvin@users.noreply.github.com>
2020-05-29 10:00:31 -0700

    Add files via upload

---
+++ farmbag/javascript.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById('mySidenav').style.width='300px';
+}
+
+function closeNav(){
+    document.getElementById('mySidenav').style.width='0px';
+    document.getElementById('mySidenav').style.left='-100px';
+}
+
+
