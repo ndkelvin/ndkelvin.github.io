b5a67643197080bca4fd527f1874754b544a5494
Ndubuisi <57354324+ndkelvin@users.noreply.github.com>
2020-06-06 05:23:35 -0700

    Update script.js

--- bioflourishing/script.js
+++ bioflourishing/script.js
@@ -1,1 +1,12 @@
-document.getElementsByClassName('jumbotron'
+var i = 0;
+var txt = 'HEALTH CARE';
+var speed = 400;
+
+function typeMsg() {
+    if (i < txt.length) {
+        document.getElementById('health-care').innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeMsg, speed);
+    }
+    setInterval(500);
+}
